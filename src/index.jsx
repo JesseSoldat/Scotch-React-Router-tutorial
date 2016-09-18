1dcfc5324cd42b0b7582b4ca507ad00d799c7396
jesse soldat <jsoldat@hotmail.com>
2016-09-18 10:24:03 +0700

    browserHistory works with a express server

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
-
+import Main from './common/Main.jsx';
 import Home from './common/Home.jsx';
 import About from './common/About.jsx';
 import Car from './car/Car.jsx';
 
 
-
-
-
 render(
-<Router history={hashHistory}>
-	<Route path="/" component={Home}/>
-	<Route path="/about" component={About}/>
-	<Route path="/car" component={Car}/>
+<Router history={browserHistory}>
+	<Route path="/" component={Main}>
+		<IndexRoute component={Home}/>
+		<Route path="/about" component={About}/>
+		<Route path="/car" component={Car}/>
+	</Route>
 
 
 </Router>, 
